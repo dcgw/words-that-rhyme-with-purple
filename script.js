f3d479231c7102912e9d2b24d7e661a52bbc4668
Daniel Cassidy <mail@danielcassidy.me.uk>
2013-07-21 23:45:20 +0100

    Where possible use requestAnimationFrame instead of setInterval.

--- script.js
+++ script.js
@@ -1,81 +1,92 @@
-(function(){
-    function addPlayButton() {
-        var game = getGame();
-        var title = getTitle();
-        var smallPrint = getSmallPrint();
-        var button = document.createElement('button');
-        button.style.position = "relative";
-        button.addEventListener("click", onClick, true);
-        button.className="play-button";
-        button.appendChild(document.createTextNode('Play Game'));
-        game.insertBefore(button, smallPrint);
-
-        function onClick() {
-            titleMusic.stop();
-            letsPlaySound.play();
-
-            button.disabled = true;
-            button.className = "play-button active";
-
-            var f = 0;
-            var interval = setInterval(function() {
-                button.style.top = Math.pow(2, f * 0.2) + 'px';
-                title.style.opacity = smallPrint.style.opacity = (1 - f/30).toString();
-
-                if (f == 10) {
-                    button.className = "play-button";
-                }
-
-                if (++f == 45) {
-                    title.style.display = "none";
-                    smallPrint.style.display = "none";
-                    button.style.display = "none";
-                    clearInterval(interval);
-
-                    gameMusic.play();
-                }
-            }, 16);
-        }
-    }
-
-    function animateTitle() {
-        var title = getTitle();
-        title.style.position = 'relative';
-
-        var f = 0;
-        setInterval(function(){
-            title.style.top = (Math.sin(f*6*Math.PI/180) * 24) + 'px';
-            title.style.lineHeight = (96 + Math.sin(3+f*5*Math.PI/180) * 6) + 'px';
-            var transform = 'scale(' + (1 + 0.1*Math.sin(9+f*2*Math.PI/180)) + ')';
-            title.style.webkitTransform = transform;
-            title.style.mozTransform = transform;
-            title.style.transform = transform;
-            ++f;
-        }, 16);
-    }
-
-    function makeSound(name, loop) {
-        var audio = document.createElement("audio");
-        var source = document.createElement("source");
-        source.src = name + ".ogg";
-        audio.appendChild(source);
-        source = document.createElement("source");
-        source.src = name + ".mp3";
-        audio.appendChild(source);
-        audio.loop = !!loop;
-        return {
-            play: function() {
-                audio.play();
+(function () {
+    var console = window.console;
+    if (!console) {
+        console = {
+            log: function () {
             },
-            stop: function() {
-                audio.pause();
+            error: function () {
             }
         }
     }
 
-    var titleMusic = makeSound("title-music", true);
-    var letsPlaySound = makeSound("lets-play", false);
-    var gameMusic = makeSound("game-music", false);
+    var time = (function () {
+        if (Date.now) {
+            return Date.now;
+        } else {
+            return function () {
+                return +new Date();
+            };
+        }
+    }());
+
+    var requestAnimationFrame;
+    var cancelAnimationFrame;
+    (function () {
+        if (window.requestAnimationFrame) {
+            requestAnimationFrame = window.requestAnimationFrame;
+            cancelAnimationFrame = window.cancelAnimationFrame;
+        } else if (window.mozRequestAnimationFrame) {
+            requestAnimationFrame = window.mozRequestAnimationFrame;
+            cancelAnimationFrame = window.mozCancelAnimationFrame;
+        } else if (window.webkitRequestAnimationFrame) {
+            requestAnimationFrame = window.webkitRequestAnimationFrame;
+            cancelAnimationFrame = window.webkitCancelAnimationFrame;
+        } else if (window.oRequestAnimationFrame) {
+            requestAnimationFrame = window.oRequestAnimationFrame;
+            cancelAnimationFrame = window.oCancelAnimationFrame;
+        } else {
+            var interval = 1000 / 60;
+            var cancelAfterNothingTimes = 60;
+            var nothingTimes = 0;
+            var intervalId;
+            var callbacksA = [];
+            var callbacksB = [];
+
+            function onInterval() {
+                var now = time();
+                var callbacks = callbacksA;
+                callbacksA = callbacksB;
+                callbacksB = callbacks;
+
+                if (callbacks.length == 0) {
+                    if (++nothingTimes >= cancelAfterNothingTimes) {
+                        nothingTimes = 0;
+                        clearInterval(intervalId);
+                        intervalId = null;
+                    }
+                } else {
+                    nothingTimes = 0;
+
+                    for (var i = 0; i < callbacks.length; ++i) {
+                        var callback = callbacks[i];
+                        if (callback) {
+                            try {
+                                callback(now);
+                            } catch (e) {
+                                console.error(e);
+                            }
+                        }
+                    }
+                }
+
+                callbacks.length = 0;
+            }
+
+            requestAnimationFrame = function (callback) {
+                if (!intervalId) {
+                    intervalId = setInterval(onInterval, interval);
+                }
+
+                var id = callbacksA.length;
+                callbacksA.push(callback);
+                return id;
+            };
+
+            cancelAnimationFrame = function (id) {
+                callbacksA[id] = null;
+            };
+        }
+    }());
 
     function getGame() {
         return document.getElementsByClassName('game')[0];
@@ -89,8 +100,90 @@
         return document.getElementsByTagName('small')[0];
     }
 
-    addPlayButton();
-    animateTitle();
+    (function (game, title, smallPrint) {
+        var playButton = document.createElement('button');
+        playButton.style.position = "relative";
+        playButton.addEventListener("click", onClick, true);
+        playButton.className = "play-button";
+        playButton.appendChild(document.createTextNode('Play Game'));
+        game.insertBefore(playButton, smallPrint);
+
+        function onClick() {
+            titleMusic.stop();
+            letsPlaySound.play();
+
+            playButton.disabled = true;
+            playButton.className = "play-button active";
+
+            var startTime = time();
+
+            function onAnimationFrame() {
+                var f = (time() - startTime) / (1000 / 60);
+
+                playButton.style.top = Math.pow(2, f * 0.2) + 'px';
+                title.style.opacity = smallPrint.style.opacity = (1 - f / 30).toString();
+
+                if (f >= 10) {
+                    playButton.className = "play-button";
+                }
+
+                if (++f < 45) {
+                    requestAnimationFrame(onAnimationFrame);
+                } else {
+                    title.style.display = "none";
+                    smallPrint.style.display = "none";
+                    playButton.style.display = "none";
+
+                    gameMusic.play();
+                }
+            }
+
+            onAnimationFrame();
+        }
+    }(getGame(), getTitle(), getSmallPrint()));
+
+    (function (title) {
+        title.style.position = 'relative';
+
+        var startTime = time();
+
+        function onAnimationFrame() {
+            var f = (time() - startTime) / (1000 / 60);
+            title.style.top = (Math.sin(f * 6 * Math.PI / 180) * 24) + 'px';
+            title.style.lineHeight = (96 + Math.sin(3 + f * 5 * Math.PI / 180) * 6) + 'px';
+            var transform = 'scale(' + (1 + 0.1 * Math.sin(9 + f * 2 * Math.PI / 180)) + ')';
+            title.style.webkitTransform = transform;
+            title.style.mozTransform = transform;
+            title.style.transform = transform;
+            requestAnimationFrame(onAnimationFrame);
+        }
+
+        onAnimationFrame();
+    }(getTitle()));
+
+    function makeSound(name, loop) {
+        var audio = document.createElement("audio");
+        var source = document.createElement("source");
+        source.src = name + ".ogg";
+        audio.appendChild(source);
+        source = document.createElement("source");
+        source.src = name + ".mp3";
+        audio.appendChild(source);
+        audio.loop = !!loop;
+        return {
+            play: function () {
+                audio.play();
+            },
+            stop: function () {
+                audio.pause();
+            }
+        }
+    }
+
+    var titleMusic = makeSound("title-music", true);
+    var letsPlaySound = makeSound("lets-play", false);
+    var gameMusic = makeSound("game-music", false);
+
     titleMusic.play();
 
     // Your earth-word ‘purple’ confuses and infuriates us!
